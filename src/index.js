0f9f19bd4e0d06217cb7698c22d3a3eaa739c02b
Andriy Bondarev <abondarev@bsdsoftware.it>
2019-08-28 18:38:35 +0200

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require("./models/Users");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const username = "abondarev";
+const password = "!Aestethyc2015";
+
+const mongoUri = `mongodb+srv://${username}:${password}@cluster-jglpo.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", () => {
+  console.error("Error connecting to mongo", error);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
